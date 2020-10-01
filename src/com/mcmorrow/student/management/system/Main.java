49dfbb9228a154b478b03fe89f50c4f5ba244072
Fearghal <Fearghal.mcmorrow@gmail.com>
2020-10-01 23:32:34 +0100

    Changed List for ArrayList

--- src/com/mcmorrow/student/management/system/Main.java
+++ src/com/mcmorrow/student/management/system/Main.java
@@ -1,5 +1,6 @@
 package com.mcmorrow.student.management.system;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -9,13 +10,13 @@ public class Main {
         System.out.println("Enter the number of new students to enroll: ");
         Scanner in = new Scanner(System.in);
         int numOfStudents = in.nextInt();
-        Student[] students = new Student[numOfStudents];
+        ArrayList<Student> students = new ArrayList<>();
 
         // Create a number of new students.
-        for(int i =0;i < students.length;i++){
-            students[i] = new Student();
-            students[i].enroll();
-            students[i].payTuition();
+        for(int i =0;i < numOfStudents;i++){
+            students.add(new Student());
+            students.get(i).enroll();
+            students.get(i).payTuition();
         }
 
         for (Student student : students) {
